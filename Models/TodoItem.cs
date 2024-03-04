148cde095be75f3e3096cfffc3532feb6ee2692e
Salahuddin <Salahuddinsaiet.10@gmail.com>
2024-03-04 11:47:00 +0200

    Added model for TodoItem

---
+++ Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoApp.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Priority { get; set; }
+    }
+}
---
+++ Pages/TodoItems/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using TodoApp.Data;
+using TodoApp.Models;
+
+namespace TodoApp.Models.Pages.TodoItems
+{
+    public class CreateModel : PageModel
+    {
+        private readonly TodoApp.Data.ApplicationDbContext _context;
+
+        public CreateModel(TodoApp.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public TodoItem TodoItem { get; set; } = default!;
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.TodoItem.Add(TodoItem);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
