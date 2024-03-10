07f4157fc32ff45347c78c9c47ae92e881be4cba
Salahuddin <Salahuddinsaiet.10@gmail.com>
2024-03-10 11:36:31 +0200

    Added authorization for access to certain pages

--- Controllers/TodoItemsController.cs
+++ Controllers/TodoItemsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -19,6 +20,7 @@ namespace TodoApp.Controllers
             _context = context;
         }
 
+        [Authorize]
         // GET: TodoItems
         public async Task<IActionResult> Index()
         {
@@ -43,6 +45,7 @@ namespace TodoApp.Controllers
             return View(todoItem);
         }
 
+        [Authorize]
         // GET: TodoItems/Create
         public IActionResult Create()
         {
